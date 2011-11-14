ae51cd7cf1dafcd303bca3173f2d2366e8a4665e
akozak <antkozak@gmail.com>
2011-11-14 13:48:59 +0300

    Replaced deprecated methods.

--- maven-plugin/src/main/java/org/eclipse/hudson/legacy/maven/plugin/MavenModuleSet.java
+++ maven-plugin/src/main/java/org/eclipse/hudson/legacy/maven/plugin/MavenModuleSet.java
@@ -55,6 +55,7 @@ import hudson.tasks.Publisher;
 import hudson.tasks.junit.JUnitResultArchiver;
 import hudson.util.CopyOnWriteMap;
 import hudson.util.DescribableList;
+import hudson.util.DescribableListUtil;
 import hudson.util.FormValidation;
 import hudson.util.Function1;
 
@@ -813,9 +814,11 @@ public class MavenModuleSet extends AbstractMavenProject<MavenModuleSet,MavenMod
         resolveDependencies = req.hasParameter( "maven.resolveDependencies" );
         processPlugins = req.hasParameter( "maven.processPlugins" );
         mavenValidationLevel = NumberUtils.toInt( req.getParameter( "maven.validationLevel" ), -1 );
-        reporters.rebuild(req,json,MavenReporters.getConfigurableList());
-        publishers.rebuild(req,json,BuildStepDescriptor.filter(Publisher.all(),this.getClass()));
-        buildWrappers.rebuild(req,json,BuildWrappers.getFor(this));
+
+        DescribableListUtil.buildFromJson(this, req, json, MavenReporters.getConfigurableList());
+        DescribableListUtil.buildFromJson(this, req, json,
+            BuildStepDescriptor.filter(Publisher.all(), this.getClass()));
+        DescribableListUtil.buildFromJson(this, req, json, BuildWrappers.getFor(this));
     }
 
     /**
