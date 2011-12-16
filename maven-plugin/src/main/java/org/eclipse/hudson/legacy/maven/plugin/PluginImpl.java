be31552a23b38086750b6902daed433bd3d7cf50
akozak <antkozak@gmail.com>
2011-12-16 14:07:16 +0300

    Added XStream aliases in maven-legacy-plugin for backward compatibility.

--- maven-plugin/src/main/java/org/eclipse/hudson/legacy/maven/plugin/PluginImpl.java
+++ maven-plugin/src/main/java/org/eclipse/hudson/legacy/maven/plugin/PluginImpl.java
@@ -18,9 +18,16 @@ package org.eclipse.hudson.legacy.maven.plugin;
 
 import hudson.Plugin;
 import hudson.XmlFile;
-import hudson.maven.MavenModuleSet;
 import hudson.model.Items;
+import hudson.model.Run;
 import org.eclipse.hudson.legacy.maven.plugin.MavenModuleSet.DescriptorImpl;
+import org.eclipse.hudson.legacy.maven.plugin.reporters.MavenAggregatedArtifactRecord;
+import org.eclipse.hudson.legacy.maven.plugin.reporters.MavenArtifact;
+import org.eclipse.hudson.legacy.maven.plugin.reporters.MavenArtifactRecord;
+import org.eclipse.hudson.legacy.maven.plugin.reporters.MavenMailer;
+import org.eclipse.hudson.legacy.maven.plugin.reporters.SurefireAggregatedReport;
+import org.eclipse.hudson.legacy.maven.plugin.reporters.SurefireArchiver;
+import org.eclipse.hudson.legacy.maven.plugin.reporters.SurefireReport;
 
 /**
  * @author huybrechts
@@ -41,6 +48,16 @@ public class PluginImpl extends Plugin {
         Items.XSTREAM.alias("dependency", ModuleDependency.class);
         Items.XSTREAM.alias("maven2-module-set", MavenModule.class);  // this was a bug, but now we need to keep it for compatibility
         Items.XSTREAM.alias("maven2-moduleset", MavenModuleSet.class);
+        Items.XSTREAM.alias("hudson.maven.reporters.MavenMailer", MavenMailer.class);
+        Items.XSTREAM.alias("hudson.maven.RedeployPublisher", RedeployPublisher.class);
+
+        Run.XSTREAM.alias("hudson.maven.MavenModuleSetBuild", MavenModuleSetBuild.class);
+        Run.XSTREAM.alias("hudson.maven.reporters.MavenAggregatedArtifactRecord", MavenAggregatedArtifactRecord.class);
+        Run.XSTREAM.alias("hudson.maven.reporters.SurefireAggregatedReport", SurefireAggregatedReport.class);
+        Run.XSTREAM.alias("hudson.maven.reporters.SurefireReport", SurefireReport.class);
+        Run.XSTREAM.alias("hudson.maven.reporters.MavenArtifactRecord", MavenArtifactRecord.class);
+        Run.XSTREAM.alias("hudson.maven.reporters.SurefireArchiver$FactoryImpl", SurefireArchiver.FactoryImpl.class);
+        Run.XSTREAM.alias("hudson.maven.reporters.MavenArtifact", MavenArtifact.class);
     }
 
 }
